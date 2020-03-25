bf2ca24d45feb7db0fa86c5eb14ec94860b9862c
Ben Cliff <bocliffwm@gmail.com>
2020-03-25 11:39:06 -0400

    Appended data to table using java

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,51 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Looping through all tableData objects
+// tableData.forEach(function(ufo_sighting) {
+//     console.log(ufo_sighting)
+// });
+
+// Getting reference to the table body
+var tbody = d3.select("tbody");
+
+// Appending table row element
+// tableData.forEach(function(ufo_sighting) {
+//     console.log(ufo_sighting);
+//     var row = tbody.append("tr");
+// });
+
+// Use Object.entries to console.log each ufo_sighting value
+// tableData.forEach(function(ufo_sighting) {
+//     console.log(ufo_sighting);
+//     var row = tbody.append("tr");
+
+//     Object.entries(ufo_sighting).forEach(function([key, value]){
+//         console.log(key, value);
+//     });
+// });
+
+// Use d3 to append 1 cell per ufo_sighting value (Date, City, Country, Shape, etc.)
+// tableData.forEach(function(ufo_sighting) {
+//     console.log(ufo_sighting);
+//     var row = tbody.append("tr");
+
+//     Object.entries(ufo_sighting).forEach(function([key, value]){
+//         console.log(key, value);
+
+//         var cell = tbody.append("td");
+//     });
+// });
+
+// Using d3 to update each cell's text with ufo_sighting values w/ fat arrow
+tableData.forEach((ufo_sighting) => {
+    
+    var row = tbody.append("tr");
+
+    Object.entries(ufo_sighting).forEach(function([key, value]){
+        console.log(key, value);
+
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
